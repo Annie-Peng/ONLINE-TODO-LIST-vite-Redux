3fc408c461c696462854be912714890de24dba2d
Annie-Peng <djs910532@gmail.com>
2023-09-06 16:53:27 +0800

    feature-apply react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import ToDoList from "./components/todolist"
+import Login from "./components/login";
+import Register from "./components/register";
+import ToDoList from "./components/todolist";
+import coverPic from "./images/cover/coverPic.png";
+import logo from "./images/cover/logo.png";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Cover = () => {
+  return (
+    <section className="bg-primary h-screen flex justify-center items-center gap-[106px] px-[116px]">
+      <div className="pic">
+        <img className="w-max-[313px] h-max-[46px] mx-auto" src={logo} />
+        <img className="w-max-[396px] h-max-[448px] mt-4" src={coverPic} />
+      </div>
+      <Outlet />
+    </section>
+  )
+}
+
 function App() {
   return (
-    <>
-      <ToDoList />
-    </>
+    <Routes>
+      <Route path="/ONLINE-TODO-LIST-vite-Redux/" element={<Cover />} >
+        <Route path="/ONLINE-TODO-LIST-vite-Redux/" element={<Login />} />
+        <Route path="/ONLINE-TODO-LIST-vite-Redux/register" element={<Register />} />
+      </Route>
+      <Route path="/ONLINE-TODO-LIST-vite-Redux/todolist" element={<ToDoList />} />
+    </Routes>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import store from './store'
-import { Provider } from 'react-redux'
+import './index.css';
+import store from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )

5bfc9bba3f146bd2355eb72d5fec3707b9863732
Annie-Peng <djs910532@gmail.com>
2023-09-08 13:44:36 +0800

    feature-create login API function and redirect to todolist

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,22 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { loginData } from "../../features/authSlice";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  function handleSubmit(e) {
+    e.preventDefault();
+    const fromData = new FormData(e.target);
+    const inputs = {
+      email: fromData.get('email'),
+      password: fromData.get('password')
+    };
+    dispatch(loginData(inputs))
+      .then(res => res && navigate('/todolist'))
+  }
   return (
-    <form className="flex flex-col gap-[24px]">
+    <form className="flex flex-col gap-[24px]" onSubmit={handleSubmit}>
       <h2 className="font-bold text-[24px]">
         最實用的線上代辦事項服務
       </h2>
@@ -13,10 +28,8 @@ export default function Login() {
         密碼
         <input className="w-full  indent-1 p-3 rounded-[10px]  font-medium placeholder:text-tertiary mt-1" id="password" name="password" placeholder="請輸入密碼" />
       </label>
-      <Link className="mx-auto" to="todolist">
-        <button className=" py-3 px-[48px] bg-secondary text-white font-bold text-[16px] rounded-[10px] w-fit mx-auto" type="button">
-          登入</button>
-      </Link>
+      <button className=" py-3 px-[48px] bg-secondary text-white font-bold text-[16px] rounded-[10px] w-fit mx-auto" type="submit">
+        登入</button>
       <button className="font-bold text-[16px] w-fit mx-auto" type="button">
         <Link to="register">註冊帳號</Link>
       </button>
